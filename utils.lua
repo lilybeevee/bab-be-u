c967c4a74d2ab0c9f489459e6ef3eeb2f3ebe434
Julien Olsson <julienolsson@Juliens-MacBook-Air.local>
2019-05-19 23:56:13 -0400

    palette stuff but actually dont use that

--- utils.lua
+++ utils.lua
@@ -340,3 +340,18 @@ function mouseOverBox(x,y,w,h)
   mousex, mousey = love.mouse.getPosition()
   return mousex > x and mousex < x+w and mousey > y and mousey < y+h
 end
+
+function HSL(h, s, l, a)
+	if s<=0 then return l,l,l,a end
+	h, s, l = h*6, s, l
+	local c = (1-math.abs(2*l-1))*s
+	local x = (1-math.abs(h%2-1))*c
+	local m,r,g,b = (l-.5*c), 0,0,0
+	if h < 1     then r,g,b = c,x,0
+	elseif h < 2 then r,g,b = x,c,0
+	elseif h < 3 then r,g,b = 0,c,x
+	elseif h < 4 then r,g,b = 0,x,c
+	elseif h < 5 then r,g,b = x,0,c
+	else              r,g,b = c,0,x
+	end return (r+m),(g+m),(b+m),a
+end
