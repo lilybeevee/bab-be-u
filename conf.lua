5d8aeed821fd46c5245b4a8b03f6ef06aee5806c
TheFox <krakaka088@gmail.com>
2019-05-24 15:18:55 +0300

    done some conf stuff

--- conf.lua
+++ conf.lua
@@ -1,5 +1,6 @@
 function love.conf(t)
     t.identity = "bab"
+    t.window.icon = "assets/sprites/bab_slep.png"
     t.version = "11.1"
     t.release = false
     t.window.title = 'bab be u'
@@ -7,4 +8,7 @@ function love.conf(t)
     t.window.vsync = false
     t.window.minwidth = 705
     t.window.minheight = 510 
+
+    t.gammacorrect = true
+    t.window.msaa = 4
 end
