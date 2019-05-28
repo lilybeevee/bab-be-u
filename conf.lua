3cbbea58eef9cef258ab4575560a9100a760f322
TheFox <krakaka088@gmail.com>
2019-05-28 15:49:44 +0300

    fixed icon a bit

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.identity = "bab"
-    t.window.icon = "assets/sprites/bab_slep.png"
+    t.window.icon = "assets/sprites/ui/baboutline.png"
     t.version = "11.1"
     t.release = false
     t.window.title = 'bab be u'
