e98ad4ff20fccff133a16692421ac3e340693c62
Timothy Stiles <Patashu@hotmail.com>
2019-06-09 11:36:54 +1000

    0 distance look at (verb) makes you match the facing dir

--- game/unit.lua
+++ game/unit.lua
@@ -108,9 +108,10 @@ function updateUnits(undoing, big_update)
       for _,stalker in ipairs(stalkers) do
         table.sort(stalkees, function(a, b) return euclideanDistance(a, stalker) < euclideanDistance(b, stalker) end )
         for _,stalkee in ipairs(stalkees) do
-          if euclideanDistance(stalker, stalkee) > 0 and testConds(stalker, stalker_conds) and testConds(stalkee, stalkee_conds) then
-            local stalk_dir = dirs8_by_offset[sign(stalkee.x - stalker.x)][sign(stalkee.y - stalker.y)]
-            if hasProperty(stalker, "orthongl") then
+          if testConds(stalker, stalker_conds) and testConds(stalkee, stalkee_conds) then
+            local dist = euclideanDistance(stalker, stalkee)
+            local stalk_dir = dist > 0 and dirs8_by_offset[sign(stalkee.x - stalker.x)][sign(stalkee.y - stalker.y)] or stalkee.dir
+            if dist > 0 and hasProperty(stalker, "orthongl") then
               local use_hori = math.abs(stalkee.x - stalker.x) > math.abs(stalkee.y - stalker.y)
               stalk_dir = dirs8_by_offset[use_hori and sign(stalkee.x - stalker.x) or 0][not use_hori and sign(stalkee.y - stalker.y) or 0]
             end
