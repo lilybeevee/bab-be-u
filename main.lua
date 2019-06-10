1519723c0fbce1b33d99e6e622597a48ae0a9b15
Cynthia <animaly.ys@gmail.com>
2019-06-10 19:51:13 +0800

    made F11 toggle borderless fullscreen

--- main.lua
+++ main.lua
@@ -241,6 +241,7 @@ function love.keypressed(key,scancode,isrepeat)
       love.window.setMode(0, 0, {borderless=false})
       fullscreen = true
     elseif fullscreen == true then
+      love.window.setMode(705, 510, {borderless=false, resizable=true, minwidth=705, minheight=510})
       fullscreen = false
     end
   elseif key == "f" and love.keyboard.isDown('lctrl') then
