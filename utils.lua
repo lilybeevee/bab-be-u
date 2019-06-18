ec68d99c0a509ed7b75e42fabbae39fe298947f5
Timothy Stiles <Timothy.Stiles@wisetechglobal.com>
2019-06-18 14:07:35 +1000

    LOOK AT dir/ortho/diag

--- utils.lua
+++ utils.lua
@@ -565,27 +565,55 @@ function testConds(unit,conds) --cond should be a {condtype,{object types},{cond
       end
     elseif condtype == "look at" then
       for _,param in ipairs(params) do
-        local others
-        if unit == outerlvl and surrounds ~= nil and surrounds_name == level_name then
-          others = {}
-          --use surrounds to remember what was around the level
-          for __,on in ipairs(surrounds[dirs8[unit.dir][1]][dirs8[unit.dir][2]]) do
-            if nameIs(on, param) then
-              table.insert(others, on);
-            end
+        print(param)
+        local isdir = false
+        if param == "ortho" then
+          isdir = true
+          if (unit.dir % 2 == 0) then
+            result = false
+            break
+          end
+        elseif param == "diag" then
+          isdir = true
+          if (unit.dir % 2 == 1) then
+            result = false
+            break
           end
         else
-          if param == "lvl" then
-            --if we're looking in-bounds, then we're looking at a level technically!
-            result = inBounds(x + dirs8[unit.dir][1], y + dirs8[unit.dir][2]);
-          elseif param ~= "mous" then
-            others = getUnitsOnTile(x + dirs8[unit.dir][1], y + dirs8[unit.dir][2], param, false, unit) --currently, conditions only work up to one layer of nesting, so the noun argument of the condition is assumed to be just a noun
-          else
-            others = getCursorsOnTile(x + dirs8[unit.dir][1], y + dirs8[unit.dir][2], false, unit)
+          for i = 1,8 do
+            if param == dirs8_by_name[i] then
+              isdir = true
+              if (unit.dir ~= i) then
+                result = false
+                break
+              end
+            end
           end
         end
-        if others ~= nil and #others == 0 then
-          result = false
+        if (not isdir) then
+          local others
+          if unit == outerlvl and surrounds ~= nil and surrounds_name == level_name then
+            others = {}
+            --use surrounds to remember what was around the level
+            for __,on in ipairs(surrounds[dirs8[unit.dir][1]][dirs8[unit.dir][2]]) do
+              if nameIs(on, param) then
+                table.insert(others, on);
+              end
+            end
+          else
+            if param == "lvl" then
+              --if we're looking in-bounds, then we're looking at a level technically!
+              result = inBounds(x + dirs8[unit.dir][1], y + dirs8[unit.dir][2]);
+            elseif param ~= "mous" then
+              others = getUnitsOnTile(x + dirs8[unit.dir][1], y + dirs8[unit.dir][2], param, false, unit) --currently, conditions only work up to one layer of nesting, so the noun argument of the condition is assumed to be just a noun
+            else
+              others = getCursorsOnTile(x + dirs8[unit.dir][1], y + dirs8[unit.dir][2], false, unit)
+            end
+          end
+          if others ~= nil and #others == 0 then
+            result = false
+            break
+          end
         end
       end
     elseif condtype == "sans" then
