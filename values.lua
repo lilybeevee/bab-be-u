961d979c8c12d47e659d5a7f38341ce0dd6876db
vitellaryjr <43586575+vitellaryjr@users.noreply.github.com>
2019-06-22 16:19:30 -0700

    added "this"

--- values.lua
+++ values.lua
@@ -2459,4 +2459,13 @@ tiles_list = {
     layer = 20,
     desc = "HAET FLOR: After movement, this unit falls UP as far as it can.",
   },
+  -- 224
+  {
+    name = "this",
+    sprite = "this",
+    grid = {16,0},
+    color = {0,3},
+    layer = 20,
+    desc = "THIS: An object that pretends like it's text."
+  },
 }
