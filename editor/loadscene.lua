28d365cb645ef55bcdf7feca601a598b364672ad
Cynthia <animaly.ys@gmail.com>
2019-07-30 15:38:41 +0800

    add fail sfx if trying to delete official world/levels

--- editor/loadscene.lua
+++ editor/loadscene.lua
@@ -458,7 +458,7 @@ end
 function scene.selectWorld(o, button)
   if button == 1 then
     if o.data.deleting then
-      o.data.deleting = nil
+      o.data.deleting = 0
       o:setColor()
       o:setSprite(sprites["ui/world box"])
     else
@@ -475,7 +475,7 @@ function scene.selectWorld(o, button)
         playSound("move")
       elseif o.data.deleting == 1 then
         o.data.deleting = 2
-        o:setSprite(sprites["ui/world box deleteconfirm"])
+        o:setSprite(sprites["ui/world box delete 2"])
         playSound("unlock")
       elseif o.data.deleting == 2 then
         deleteDir(o.data.file .. "/" .. o:getName())
@@ -483,6 +483,8 @@ function scene.selectWorld(o, button)
         shakeScreen(0.3, 0.1)
         scene.buildUI()
       end
+    else
+      playSound("fail")
     end
   end
 end
@@ -547,7 +549,7 @@ function scene.selectLevel(o, button)
         playSound("move")
       elseif o.data.deleting == 1 then
         o.data.deleting = 2
-        o:setSprite(sprites["ui/level box deleteconfirm"])
+        o:setSprite(sprites["ui/level box delete 2"])
         playSound("unlock")
       elseif o.data.deleting == 2 then
         local dir = "levels/"
@@ -558,6 +560,8 @@ function scene.selectLevel(o, button)
         shakeScreen(0.3, 0.1)
         scene.buildUI()
       end
+    else
+      playSound("fail")
     end
   end
 end
