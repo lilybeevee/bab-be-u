da9597bc8a79703f798edc23a85dd92cd6ec1ac9
Notnat Tlikehis <notnat.tlikehis@gmail.com>
2019-08-31 13:58:39 -0700

    ignore & in rule deduplication

--- game/rules.lua
+++ game/rules.lua
@@ -479,7 +479,7 @@ function parseSentence(sentence_, params_, dir) --prob make this a local functio
           if other.dir == full_rule.dir then
             local subset = true
             for _,u in ipairs(other.units) do
-              if not full_rule.units_set[u] then 
+              if not full_rule.units_set[u] and not u.texttype["and"] then 
                 subset = false
                 break
               end
@@ -489,7 +489,7 @@ function parseSentence(sentence_, params_, dir) --prob make this a local functio
             else
               local subset = true
               for _,u in ipairs(full_rule.units) do
-                if not other.units_set[u] then
+                if not other.units_set[u] and not u.texttype["and"] then
                   subset = false
                   break
                 end
