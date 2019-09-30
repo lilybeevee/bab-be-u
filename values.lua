b2bcfc37bcf148acc5d12ab93344859b55b72f58
danielbupisgamer <masterofthediamondsword@gmail.com>
2019-09-30 16:07:48 -0400

    please update for err (#66)
    eye ajustment would be great
    (modified a bit, please take note of what i changed)

--- values.lua
+++ values.lua
@@ -253,7 +253,7 @@ selector_grid_contents = {
     "han","text_han","iy","text_iy","lisp","text_lisp","paw","text_paw",0,0,0,0,0,0,"larry","text_larry","zsoob","text_zsoob",
     "snoman","text_snoman","pingu","text_pingu",0,0,0,0,0,0,0,0,0,0,0,0,"o","text_o",
     "kapa","text_kapa","urei","text_urei","ryugon","text_ryugon",0,0,0,0,0,0,0,0,0,0,"square","text_square",
-    "os","text_os","hors","text_hors",0,0,0,0,0,0,0,0,0,0,0,0,"triangle","text_triangle",
+    "os","text_os","hors","text_hors","err","text_err",0,0,0,0,0,0,0,0,0,0,"triangle","text_triangle",
   },
   -- page 5: inanimate objects
   {
@@ -7056,7 +7056,7 @@ tiles_list = {
     tags = {"chars", "cop", "police", "cat"},
     desc = "u hav da wight to wemain siwent!!",
   },
-  -- 609
+ -- 609
   {
     name = "kat",
     sprite = "kat",
@@ -7179,6 +7179,27 @@ tiles_list = {
     layer = 20,
     tags = {"fluff", "floof", "brother", "dog"},
     desc = "not a flog",
+  },
+  -- 618
+  {
+    name = "err",
+    sprite = "err",
+    color = {0, 3},
+    layer = 6,
+    rotate = true,
+    eye = {x=20, y=5, w=2, h=2},
+    tags = {"chars", "error"},
+    desc = "kinda spooky in bab tbh",
+  },
+  -- 619
+  {
+    name = "text_err",
+    sprite = "text_err",
+    type = "text",
+    texttype = {object = true},
+    color = {0,3},
+    layer = 20,
+    tags = {"chars", "error"},
   },
 }
 
