65bf2ac4fa86798de80a2043d9da2f4f4e1dfbe5
Jill <animaly.ys@gmail.com>
2022-12-01 23:05:53 +0800

    added the word loss to mute on focus option

--- utils.lua
+++ utils.lua
@@ -16,7 +16,6 @@ function clear()
     replay_string = ""
   end
   rhythm_time = love.timer.getTime()
-  rhythm_interval = settings["rhythm_interval"]
   rhythm_queued_movement = {0, 0, "wait"}
   new_units_cache = {}
   undoing = false
@@ -3793,7 +3792,7 @@ function buildOptions()
     scene.addOption("music_vol", "music volume", {{"25%", 0.25}, {"50%", 0.5}, {"75%", 0.75}, {"100%", 1}})
     scene.addOption("sfx_on", "sound", {{"on", true}, {"off", false}})
     scene.addOption("sfx_vol", "sound volume", {{"25%", 0.25}, {"50%", 0.5}, {"75%", 0.75}, {"100%", 1}})
-    scene.addOption("focus_sound", "mute on focus", {{"on", false}, {"off", true}})
+    scene.addOption("focus_sound", "mute on focus loss", {{"on", false}, {"off", true}})
     scene.addButton("back", function() global_menu_state = "none"; scene.buildUI() end)
   elseif global_menu_state == "video" then
     scene.addOption("int_scaling", "integer scaling", {{"on", true}, {"off", false}})
